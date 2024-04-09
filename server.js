fc4317082047a19fb7d6ec351ea45c2d7a2f9d88
eliezeribghi <120668513+eliezeribghi@users.noreply.github.com>
2024-04-09 22:30:02 +0200

    v

--- server.js
+++ server.js
@@ -1,57 +1,10 @@
 import express from 'express';
 import compression from 'compression'; // Importez le module compression
-import { render } from 'svelte-email';
-import ContactEmail from '$lib/emails/ContactEmail.svelte';
-import nodemailer from 'nodemailer';
+
+
 
 const app = express();
 
 // Utilisez le middleware compression
 app.use(compression());
 
-// Définissez vos routes après avoir configuré le middleware compression
-app.post('/contact', async (req, res) => {
-  const { name, lastname, email, phoneNumber, subject, message } = req.body;
-
-  const emailHtml = render({
-    component: ContactEmail,
-    props: {
-      name,
-      lastname,
-      email,
-      phoneNumber,
-      subject,
-      message
-    }
-  });
-
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'my_user',
-      pass: 'my_password'
-    }
-  });
-
-  const options = {
-    from: 'you@example.com',
-    to: 'user@gmail.com', // Remplacez par votre adresse e-mail de destination
-    subject: 'Nouvelle soumission de formulaire de contact',
-    html: emailHtml
-  };
-
-  try {
-    const result = await transporter.sendMail(options);
-    console.log('Email envoyé :', result);
-    res.status(200).json({ message: 'Email envoyé avec succès' });
-  } catch (error) {
-    console.error('Erreur lors de l\'envoi de l\'e-mail :', error);
-    res.status(500).json({ error: 'Erreur interne du serveur' });
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Serveur démarré sur le port 3000');
-});
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import vue from "@vitejs/plugin-vue";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import compression from "vite-plugin-compression";
 import { copy } from 'vite-plugin-copy'; // Importer le plugin copy
+import assets from 'vite-plugin-assets';
 
 export default defineConfig({
   plugins: [
     vue(),
     svelte(),
+    assets({
+      input: 'src/assets', // le dossier où vos images sont stockées
+      output: 'dist/assets', // le dossier où vous voulez que vos images soient copiées
+    }),
     compression({
       verbose: true,
       filter: /\.(js|css|json|txt|html|svg)$/,
